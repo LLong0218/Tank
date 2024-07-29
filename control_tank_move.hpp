ecdd5f95fe437a49fcc81d2d310410346b5c8dac
哲旭 龙 <337104135@qq.com>
2024-07-29 18:22:27 +0800

    更新代码

--- control_tank_move.hpp
+++ control_tank_move.hpp
@@ -1,54 +1,53 @@
 #pragma once
 
-int control_tank_move(Tank* enemy_tank, Direction direction, IMAGE* enemy_img) {
-    switch (direction) {
+int control_tank_move(Tank* tank, Direction direction, IMAGE* img) {
+    switch (tank->tank_direction) {
    
     case UP:
         //ÏòÉÏÒÆ¶¯
-        if (direction == UP && enemy_tank->y - 1 >= 0 && map[enemy_tank->y - 1][enemy_tank->x] == 0 && map[enemy_tank->y - 1][enemy_tank->x + 1] == 0)
+        if (tank->tank_direction == UP && tank->y - 1 >= 0 && map[tank->y - 1][tank->x] == 0 && map[tank->y - 1][tank->x + 1] == 0)
         {
-
-            tank_move(enemy_tank, UP, enemy_img, 1);
+            tank_move(tank, UP, img, 1);
         }
-        else if (direction != UP) {
-            direction = UP;
-            tank_move(enemy_tank, UP, enemy_img, 0);
+        else if (tank->tank_direction != UP) {
+            tank->tank_direction = UP;
+            tank_move(tank, UP, img, 0);
         }
 
         break;
     case LEFT:
-        if (direction == LEFT && enemy_tank->x - 1 >= 0 && map[enemy_tank->y][enemy_tank->x - 1] == 0 && map[enemy_tank->y + 1][enemy_tank->x - 1] == 0)
+        if (tank->tank_direction == LEFT && tank->x - 1 >= 0 && map[tank->y][tank->x - 1] == 0 && map[tank->y + 1][tank->x - 1] == 0)
         {
 
-            tank_move(enemy_tank, LEFT, enemy_img, 1);
+            tank_move(tank, LEFT, img, 1);
         }
-        else if (direction != LEFT) {
-            direction = LEFT;
-            tank_move(enemy_tank, LEFT, enemy_img, 0);
+        else if (tank->tank_direction != LEFT) {
+            tank->tank_direction = LEFT;
+            tank_move(tank, LEFT, img, 0);
         }
         break;
     case DOWN:
-        if (direction == DOWN && enemy_tank->y + 2 <= 25 && map[enemy_tank->y + 2][enemy_tank->x] == 0 && map[enemy_tank->y + 2][enemy_tank->x + 1] == 0)
+        if (tank->tank_direction == DOWN && tank->y + 2 <= 25 && map[tank->y + 2][tank->x] == 0 && map[tank->y + 2][tank->x + 1] == 0)
         {
 
-            tank_move(enemy_tank, DOWN, enemy_img, 1);
+            tank_move(tank, DOWN, img, 1);
 
         }
-        else if (direction != DOWN) {
-            direction = DOWN;
-            tank_move(enemy_tank, DOWN, enemy_img, 0);
+        else if (tank->tank_direction != DOWN) {
+            tank->tank_direction = DOWN;
+            tank_move(tank, DOWN, img, 0);
         }
         break;
     case RIGHT:
-        if (direction == RIGHT && enemy_tank->x + 2 <= 25 && map[enemy_tank->y][enemy_tank->x + 2] == 0 && map[enemy_tank->y + 1][enemy_tank->x + 2] == 0)
+        if (tank->tank_direction == RIGHT && tank->x + 2 <= 25 && map[tank->y][tank->x + 2] == 0 && map[tank->y + 1][tank->x + 2] == 0)
         {
 
-            tank_move(enemy_tank, RIGHT, enemy_img, 1);
+            tank_move(tank, RIGHT, img, 1);
 
         }
-        else if (direction != RIGHT) {
-            direction = RIGHT;
-            tank_move(enemy_tank, RIGHT, enemy_img, 0);
+        else if (tank->tank_direction != RIGHT) {
+            tank->tank_direction = RIGHT;
+            tank_move(tank, RIGHT, img, 0);
         }
         break;
         return 1;
